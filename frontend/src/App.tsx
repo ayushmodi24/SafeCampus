82e30b26370a5fa3e4a722ec55cfd62dbd204fa7
ayushmodi24 <modiayush908@gmail.com>
2025-09-23 11:47:07 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import HomePage from "./components/HomePage"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import Chatbot from "./components/Chatbot";
+import LearnSafety from "./components/LearnSafety";
 function App() {
   return (
     <div className=''>
@@ -9,12 +10,13 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage/>} />
-          <Route path="/safety" element={<HomePage/>} />
+          <Route path="/safety" element={<LearnSafety/>} />
           <Route path="/chatbot" element={<Chatbot />} />
         </Routes>
       </Router>
     </div>
   )
 }
+
 
 export default App
